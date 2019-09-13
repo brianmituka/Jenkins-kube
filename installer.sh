8e2a380b87f3d794ff3f97162131dbd9407aafce
brian <mitukab@outlook.com>
2019-09-14 00:01:40 +0300

    Added method for downloading kubectl for mac and linux

--- installer.sh
+++ installer.sh
@@ -25,7 +25,12 @@ install_kubectl(){
         echo "Installing on mac"
         brew install kubernetes-cli
     else
-        echo "Installing on linux"    
+        echo "Installing on linux" 
+        curl -LO https://storage.googleapis.com/kubernetes-release/release \ 
+        $(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt) \
+        /bin/linux/amd64/kubectl && chmod +x ./kubectl && \
+        sudo mv ./kubectl /usr/local/bin/kubectl
+
     fi    
 
 }
