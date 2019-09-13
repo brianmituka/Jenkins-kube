a812c6447ae7fb87f6d68e1ec84f61cc16583ca4
brian <mitukab@outlook.com>
2019-09-13 23:50:55 +0300

    Added script to check if dependencies are available

--- installer.sh
+++ installer.sh
@@ -4,3 +4,61 @@
 #install virtualbox
 #install kubectl
 #install docker
+osType=$(uname)
+        
+install_minikube(){
+    if ["$osType" == "Darwin"]; then
+        echo "Installing on mac"
+    else
+        echo "Installing on linux"    
+    fi    
+
+}
+
+install_virtualbox(){
+    echo "Installing virtual box"
+}
+
+install_kubectl(){
+    echo "Installing Kubectl"
+    if ["$osType" == "Darwin"]; then
+        echo "Installing on mac"
+        brew install kubernetes-cli
+    else
+        echo "Installing on linux"    
+    fi    
+
+}
+
+install_docker (){
+    echo "Installing Docker"
+}
+
+
+if ! [ -x "$(command -v minikube)" ]; then
+    echo "minikube not installed installing...."
+    install_minikube
+else
+    echo "Minikube already installed exiting"
+fi
+
+if ! [ -x "$(command -v virtualbox)" ]; then
+    echo "virtualbox not installed installing...."
+    install_virtualbox
+else
+    echo "virtualbox already installed exiting"
+fi
+
+if ! [ -x "$(command -v kubectl)" ]; then
+    echo "kubectl not installed installing...."
+    install_kubectl
+else
+    echo "kubectl already installed exiting"
+fi
+
+if ! [ -x "$(command -v docker)" ]; then
+    echo "docker not installed installing...."
+    install_docker
+else
+    echo "docker already installed exiting"
+fi
